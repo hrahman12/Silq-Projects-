d772ca64c54aaf50e899246d6ac3c4ed8471b6ce
hrahman12 <33207831+hrahman12@users.noreply.github.com>
2022-02-02 23:13:06 -0500

    Delete Linear Search Algo.py

--- Linear Search Algo.py
+++
@@ -1,8 +0,0 @@
-a = ['t', 'u', 't', 'o', 'r', 'i', 'a', 'l']
-x='o'
-def linearsearch(a,x):
-    for i in range(len(a)):
-        if a[i]==x:
-            return i
-    return -1
-print("element found at index " +str(linearsearch(a,x)))
